aa4cc661948f23e83835aa87ce3f027837cd8ece
taariqq <taariqq@gmail.com>
2020-01-14 12:24:29 +0500

    working ...

--- gcd_start.py
+++ gcd_start.py
@@ -8,9 +8,34 @@ def gcd(a, b):
       a = b
       b = t % b
       
-    return a
+    return a, b
     
         
 # try out the function with a few examples
+# step 1, a = 96; b = 60
+# step 2, t = 60; a = 60; b = 36
+# step 3, t = 60; a = 36; b = 24
+# step 4, t = 36; a = 24; b = 12
+# step 5, t = 24; a = 12; b = 12
+# step 6, t = 12; a = 12; b = 0 -- done!
 print(gcd(60, 96))  # should be 12
+# step 1, t = 20; a = 8; b = 4
+# step 2, t = 8;  a = 4; b = 0 -- done!
 print(gcd(20, 8))   # should be 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
