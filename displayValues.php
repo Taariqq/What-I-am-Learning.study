615ef8517e09f6401b7c4b5927786b8f66374282
taariqq <taariqq@gmail.com>
2020-01-17 16:32:15 +0500

    finish variable intro

---
+++ displayValues.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+display values
+ */
+$firstName = 'David';
+$firstName = 'David';
+//$first-Name = 'David';
+//$1firstName = 'David';
+$number = 4200.99;
+
+echo  $number, ' ', $firstName;
+//print $number;
